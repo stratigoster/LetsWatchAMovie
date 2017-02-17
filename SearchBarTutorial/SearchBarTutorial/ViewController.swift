f48fe06e0cc75462056252f98bc460259f2a03f1
stratigo <williamicdc@gmail.com>
2017-02-17 16:24:31 -0500

    added ability to undo lackening of screen by tapping once

--- SearchBarTutorial/SearchBarTutorial/ViewController.swift
+++ SearchBarTutorial/SearchBarTutorial/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let blackView = UIView()
+    
     @IBOutlet weak var moreButton: UIBarButtonItem!
     @IBOutlet weak var searchBar: UISearchBar!
     
@@ -17,21 +19,24 @@ class ViewController: UIViewController {
         
         if let window = UIApplication.shared.keyWindow {
             
-            let blackView = UIView()
-            
             blackView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+            
+            blackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDismiss)))
             
             window.addSubview(blackView)
             blackView.frame = window.frame
             blackView.alpha = 0
             
             UIView.animate(withDuration: 0.5, animations: {
-                blackView.alpha = 1
+                self.blackView.alpha = 1
             })
-            
         }
-        
-        
+    }
+     
+    func handleDismiss() {
+        UIView.animate(withDuration: 0.5) {
+            self.blackView.alpha = 0
+        }
     }
     
     override func viewDidLoad() {
