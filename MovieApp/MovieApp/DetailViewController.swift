7414f8ef25865f9d4cb051ec682bdb91daedb988
stratigo <williamicdc@gmail.com>
2017-03-25 00:22:45 -0400

    added stub files

---
+++ MovieApp/MovieApp/DetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DetailViewController.swift
+//  MovieApp
+//
+//  Created by xszhao on 2017-03-25.
+//  Copyright © 2017 xszhao. All rights reserved.
+//
+
+import Foundation
---
+++ MovieApp/MovieApp/MenuBar.swift
@@ -0,0 +1,9 @@
+//
+//  MenuBar.swift
+//  MovieApp
+//
+//  Created by xszhao on 2017-03-25.
+//  Copyright © 2017 xszhao. All rights reserved.
+//
+
+import Foundation
